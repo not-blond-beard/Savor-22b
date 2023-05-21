8429310a2f24b014f41a50eb5f69f059f6d38ae9
upa-r-upa <duekthtjf783@gmail.com>
2023-05-22 02:45:13 +0900

    feat: Add base csv model skeleton

---
+++ backend/app/Savor22b/Model/RecipeStat.cs
@@ -0,0 +1,9 @@
+namespace Savor22b.Model;
+
+public class RecipeStat
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string MinGrade { get; set; }
+    public string MaxGrade { get; set; }
+}
