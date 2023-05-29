e6e211bda5e52df4bed3b4825dc5af277e6ed306
upa-r-upa <duekthtjf783@gmail.com>
2023-05-29 18:32:46 +0900

    feat: Update stat model & CSV data

--- backend/app/Savor22b/Model/Stat.cs
+++ backend/app/Savor22b/Model/Stat.cs
@@ -10,4 +10,8 @@ public class Stat
     public int MaxHP { get; set; }
     public int MinDef { get; set; }
     public int MaxDef { get; set; }
+    public int MinAtk { get; set; }
+    public int MaxAtk { get; set; }
+    public int MinSpd { get; set; }
+    public int MaxSpd { get; set; }
 }
