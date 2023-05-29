16c21aeffb037565ed7ff69fa11be2421303fc53
upa-r-upa <duekthtjf783@gmail.com>
2023-05-29 20:17:31 +0900

    Update game CSV and model

--- backend/app/Savor22b/Model/Stat.cs
+++ backend/app/Savor22b/Model/Stat.cs
@@ -3,8 +3,8 @@ namespace Savor22b.Model;
 public class Stat
 {
     public int ID { get; set; }
-    public int IngredientID { get; set; }
-    public int RecipeID { get; set; }
+    public int? IngredientID { get; set; }
+    public int? RecipeID { get; set; }
     public string Grade { get; set; }
     public int MinHP { get; set; }
     public int MaxHP { get; set; }
