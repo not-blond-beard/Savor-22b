22ca2ab59597a5bb7c7e7468a752a930cb524655
kth1888 <kth1888@gmail.com>
2023-07-25 02:14:23 +0900

    feat: Create Seed

---
+++ frontend/Savor-22b/Assets/Scripts/CreateSeed.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GraphQlClient.Core;
+using GraphQlClient.EventCallbacks;
+using UnityEngine.Networking;
+
+using TMPro; // Textmeshpro
+
+
+public class CreateSeed : MonoBehaviour
+{
+    public string pkHex = "eda6ef63ae945cd15572fcf7d6635a8b3f8d86e85b57a353b482bc82c7fd2ad4";
+
+    public GameObject loading;
+
+    public GraphApi SavorReference;
+
+    public TMP_InputField inputString;
+    public string newSeedName;
+    public TMP_Text mutationDisplay;
+
+    public async void createNewSeed()
+    {
+        loading.SetActive(true);
+	//Gets the needed query from the Api Reference
+        GraphApi.Query createSeed = SavorReference.GetQueryByName("CreateNewSeed", GraphApi.Query.Type.Mutation);
+	
+	//Converts the JSON object to an argument string and sets the queries argument
+        createSeed.SetArgs(new{privateKeyHex = "eda6ef63ae945cd15572fcf7d6635a8b3f8d86e85b57a353b482bc82c7fd2ad4"});
+	
+	//Performs Post request to server
+        UnityWebRequest request = await SavorReference.Post(createSeed);
+
+        loading.SetActive(false);
+
+        mutationDisplay.text = HttpHandler.FormatJson(request.downloadHandler.text);
+    
+    }
+
+    public void applyInput()
+    {
+        newSeedName = inputString.GetComponent<TMP_InputField>().text;
+    }
+
+
+}
