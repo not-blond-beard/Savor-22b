a592babea92e14071882c26c5542d357c1437b05
Atralupus <me@atralupus.com>
2023-08-14 00:41:29 +0900

    feat: Introduce CsvDataHelper

---
+++ backend/app/Savor22b/Constants/CsvDataFileNames.cs
@@ -0,0 +1,15 @@
+namespace Savor22b.Constants;
+
+public static class CsvDataFileNames
+{
+    public static readonly string Seed = "seed.csv";
+    public static readonly string Ingredient = "ingredient.csv";
+    public static readonly string Food = "food.csv";
+    public static readonly string Recipe = "recipe.csv";
+    public static readonly string CookingEquipment = "cooking_equipment.csv";
+    public static readonly string CookingEquipmentCategory = "cooking_equipment_category.csv";
+    public static readonly string Item = "item.csv";
+    public static readonly string Village = "villages.csv";
+    public static readonly string VillageCharacteristics = "village_characteristics.csv";
+    public static readonly string Stat = "stat.csv";
+}
---
+++ backend/app/Savor22b/Savor22bConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Libplanet.Headless;
+
+
+public class Savor22bConfiguration : Configuration
+{
+    public string CsvDataResourcePath { get; init; }
+}
