4c08a6b88f2c56fd6122aa9bca522c3e3f38b159
upa-r-upa <duekthtjf783@gmail.com>
2023-08-14 04:12:15 +0900

    feat: Remove prev action

--- backend/app/Savor22b/GraphTypes/Query.cs
+++ backend/app/Savor22b/GraphTypes/Query.cs
@@ -122,34 +122,6 @@ public class Query : ObjectGraphType
         );
 
         Field<NonNullGraphType<StringGraphType>>(
-            "createAction_GenerateIngredient",
-            description: "Generate Ingredient",
-            arguments: new QueryArguments(
-                new QueryArgument<NonNullGraphType<StringGraphType>>
-                {
-                    Name = "publicKey",
-                    Description = "The base64-encoded public key for Transaction.",
-                },
-                new QueryArgument<NonNullGraphType<GuidGraphType>>
-                {
-                    Name = "seedStateId",
-                    Description = "Seed state Id (PK)",
-                }
-            ),
-            resolve: context =>
-            {
-                var publicKey = new PublicKey(ByteUtil.ParseHex(context.GetArgument<string>("publicKey")));
-
-                var action = new GenerateIngredientAction(
-                    context.GetArgument<Guid>("seedStateId"),
-                    Guid.NewGuid()
-                );
-
-                return getUnsignedTransactionHex(action, publicKey);
-            }
-        );
-
-        Field<NonNullGraphType<StringGraphType>>(
             "createAction_GenerateFood",
             description: "Generate Food",
             arguments: new QueryArguments(
