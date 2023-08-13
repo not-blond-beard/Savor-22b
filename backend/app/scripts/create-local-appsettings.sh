a14e982b4a19b7a60c6df571a19e13edd46201e0
Atralupus <me@atralupus.com>
2023-08-14 03:47:19 +0900

    fix: create local settings script
    fix typo
    
    find last index
    
    fix
    
    if action

--- backend/app/scripts/create-local-appsettings.sh
+++ backend/app/scripts/create-local-appsettings.sh
@@ -2,16 +2,21 @@
 
 current_path=$(pwd)
 root_path="$current_path"
-while [[ ! "$root_path" =~ "Savor-22b" ]]; do
-  root_path=$(dirname "$root_path")
-done
-root_path=${root_path%%/Savor-22b*}
 
-project_path="$root_path/Savor-22b/backend/app"
+if [[ "$current_path" == "/home/runner/work/Savor-22b/Savor-22b"* ]]; then
+  root_path="/home/runner/work/Savor-22b/Savor-22b"
+else
+  while [[ "$root_path" == *"/Savor-22b"* ]]; do
+    root_path=${root_path%"/Savor-22b"*}
+  done
+  root_path="$root_path/Savor-22b"
+fi
+
+project_path="$root_path/backend/app"
 
 genesis_block_path="file://$project_path/data/genesis.bin"
 store_uri="rocksdb+file://$project_path/data/store?secure=false"
-csv_data_resource_path="$root_path/Savor-22b/resources/savor22b/tabledata"
+csv_data_resource_path="$root_path/resources/savor22b/tabledata"
 
 local_json_file="$project_path/Savor22b/appsettings.local.json"
 json_file="$project_path/Savor22b/appsettings.json"
