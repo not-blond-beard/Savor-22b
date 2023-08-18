9e28fc5261b74611768e6b8e2e274fc8c0840715
upa-r-upa <duekthtjf783@gmail.com>
2023-08-19 01:27:39 +0900

    feat: Add World CSV & Update Village CSV

--- backend/app/Savor22b/Model/Village.cs
+++ backend/app/Savor22b/Model/Village.cs
@@ -6,5 +6,6 @@ public class Village
     public string Name { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
-    public int DistanceFromCenter { get; set; }
+    public int WorldX { get; set; }
+    public int WorldY { get; set; }
 }
---
+++ backend/app/Savor22b/Model/World.cs
@@ -0,0 +1,8 @@
+namespace Savor22b.Model;
+
+public class World
+{
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+}
