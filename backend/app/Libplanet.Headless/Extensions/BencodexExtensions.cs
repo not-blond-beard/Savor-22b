abb81e1f9c1a4c939802a39029482ae4ea7f5f15
Atralupus <me@atralupus.com>
2023-08-20 03:18:55 +0900

    feat: Add nullable Se/Deserialize types

--- backend/app/Libplanet.Headless/Extensions/BencodexExtensions.cs
+++ backend/app/Libplanet.Headless/Extensions/BencodexExtensions.cs
@@ -6,6 +6,19 @@ namespace Libplanet.Headless.Extensions;
 
 public static class BencodexExtensions
 {
+
+    public static IValue Serialize<T>(Func<T, IValue> serializer, T? value)
+        where T : struct
+    {
+        return value is T v ? serializer(v) : Null.Value;
+    }
+
+    public static T? Deserialize<T>(Func<IValue, T> deserializer, IValue serialized)
+        where T : struct
+    {
+        return serialized is Null ? (T?)null : deserializer(serialized);
+    }
+
     public static IValue ToBencodex(this FungibleAssetValue fav) =>
         new List(fav.Currency.Serialize(), (Integer)fav.RawValue);
 
@@ -27,8 +40,14 @@ public static class BencodexExtensions
     public static IValue Serialize(this Guid number) =>
         new Binary(number.ToByteArray());
 
+    public static IValue Serialize(this Guid? number) =>
+        Serialize(Serialize, number);
+
     public static Guid ToGuid(this IValue serialized) =>
         new Guid(((Binary)serialized).ToByteArray());
+
+    public static Guid? ToNullableGuid(this IValue serialized) =>
+        Deserialize(ToGuid, serialized);
 
     #endregion Guid
 
@@ -37,8 +56,14 @@ public static class BencodexExtensions
     public static IValue Serialize(this int number) =>
         (Text)number.ToString(CultureInfo.InvariantCulture);
 
+    public static IValue Serialize(this int? number) =>
+        Serialize(Serialize, number);
+
     public static int ToInteger(this IValue serialized) =>
         int.Parse(((Text)serialized).Value, CultureInfo.InvariantCulture);
+
+    public static int? ToNullableInteger(this IValue serialized) =>
+        Deserialize(ToInteger, serialized);
 
     #endregion int
 
@@ -47,8 +72,14 @@ public static class BencodexExtensions
     public static IValue Serialize(this long number) =>
         (Text)number.ToString(CultureInfo.InvariantCulture);
 
+    public static IValue Serialize(this long? number) =>
+        Serialize(Serialize, number);
+
     public static long ToLong(this IValue serialized) =>
         long.Parse(((Text)serialized).Value, CultureInfo.InvariantCulture);
+
+    public static long? ToNullableLong(this IValue serialized) =>
+        Deserialize(ToLong, serialized);
 
     #endregion long
 }
