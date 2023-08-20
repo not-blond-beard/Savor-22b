b97dfaee48529ee29414ad06720f5acb5eb5a0ae
upa-r-upa <duekthtjf783@gmail.com>
2023-08-20 09:35:47 +0900

    fix: Fix CSV path recognition bug

--- backend/app/Savor22b/Program.cs
+++ backend/app/Savor22b/Program.cs
@@ -26,29 +26,31 @@ var app = CoconaApp.Create();
 app.AddCommand(
     "execute",
     (
-        [Argument(Description = "The private key of the validator.  If not given, the node will not " +
-            "validate blocks.  You can create a new private key with the `dotnet planet key create` " +
-            "command.  You can list the created keys in your machine with `dotnet planet key` " +
-            "command.")]
-        Guid? validatorKeyId,
+        [Argument(
+            Description = "The private key of the validator.  If not given, the node will not "
+                + "validate blocks.  You can create a new private key with the `dotnet planet key create` "
+                + "command.  You can list the created keys in your machine with `dotnet planet key` "
+                + "command."
+        )]
+            Guid? validatorKeyId,
         PassphraseParameters passphrase
     ) =>
     {
         // Get configuration
-        string configPath = Environment.GetEnvironmentVariable("SAVOR22B_CONFIG_FILE") ?? "appsettings.json";
+        string configPath =
+            Environment.GetEnvironmentVariable("SAVOR22B_CONFIG_FILE") ?? "appsettings.json";
 
         var configurationBuilder = new ConfigurationBuilder()
             .AddJsonFile(configPath)
             .AddEnvironmentVariables("SAVOR22B_");
         IConfiguration config = configurationBuilder.Build();
 
-        var loggerConf = new LoggerConfiguration()
-        .ReadFrom.Configuration(config);
+        var loggerConf = new LoggerConfiguration().ReadFrom.Configuration(config);
         Log.Logger = loggerConf.CreateLogger();
 
         var svrConfig = new Savor22bConfiguration();
-        CsvDataHelper.Initialize(svrConfig.CsvDataResourcePath);
         config.Bind(svrConfig);
+        CsvDataHelper.Initialize(svrConfig.CsvDataResourcePath);
 
         PrivateKey? validatorKey = null;
         if (validatorKeyId is { } keyId)
@@ -74,9 +76,11 @@ app.AddCommand(
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.BackgroundColor = ConsoleColor.DarkBlue;
                 Console.WriteLine(new string('-', Console.WindowWidth));
-                Console.WriteLine("NOTE: The node is running in the solo validaton mode. The swarm " +
-                    "is not running, and the node will not be able to sync blocks to remote nodes. " +
-                    $"If this is not what you want, configure the \"Network\" section of {configPath}.");
+                Console.WriteLine(
+                    "NOTE: The node is running in the solo validaton mode. The swarm "
+                        + "is not running, and the node will not be able to sync blocks to remote nodes. "
+                        + $"If this is not what you want, configure the \"Network\" section of {configPath}."
+                );
                 Console.Write(new string('-', Console.WindowWidth));
                 Console.ResetColor();
                 Console.WriteLine();
@@ -89,9 +93,11 @@ app.AddCommand(
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.DarkRed;
             Console.WriteLine(new string('-', Console.WindowWidth));
-            Console.WriteLine("WARNING: The validator key is not given.  The node will not validate " +
-                "blocks.  If it is intended, ignore this message.  If you want to validate blocks, " +
-                "you first need to create a new private key with the following command:");
+            Console.WriteLine(
+                "WARNING: The validator key is not given.  The node will not validate "
+                    + "blocks.  If it is intended, ignore this message.  If you want to validate blocks, "
+                    + "you first need to create a new private key with the following command:"
+            );
             Console.WriteLine();
             Console.WriteLine("\tdotnet planet key create");
             Console.WriteLine();
@@ -99,9 +105,11 @@ app.AddCommand(
             Console.Write(new string('-', Console.WindowWidth));
             if (svrConfig.Network is null)
             {
-                Console.WriteLine("As the network is not configured, this node will not be able to " +
-                    "see any new blocks. If you intended to sync blocks from remote nodes, please " +
-                    "configure the network.");
+                Console.WriteLine(
+                    "As the network is not configured, this node will not be able to "
+                        + "see any new blocks. If you intended to sync blocks from remote nodes, please "
+                        + "configure the network."
+                );
                 Console.Write(new string('-', Console.WindowWidth));
             }
             Console.ResetColor();
@@ -118,12 +126,7 @@ app.AddCommand(
                 builder
                     .UseConfiguration(svrConfig)
                     .UseActionLoader(new SVRActionLoader())
-                    .UseRenderers(
-                        new List<IRenderer>
-                        {
-                            renderer
-                        }
-                    )
+                    .UseRenderers(new List<IRenderer> { renderer })
                     .UseBlockPolicy(BlockPolicySource.GetPolicy());
 
                 if (validatorKey is { } key)
@@ -156,11 +159,10 @@ app.AddCommand(
 
         if (svrConfig.GraphQLUri is { } graphqlUri)
         {
-            builder.WebHost
-                .ConfigureKestrel(options =>
-                {
-                    options.Listen(IPAddress.Parse(graphqlUri.DnsSafeHost), graphqlUri.Port);
-                });
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Parse(graphqlUri.DnsSafeHost), graphqlUri.Port);
+            });
         }
 
         using WebApplication app = builder.Build();
@@ -188,37 +190,39 @@ app.AddCommand(
     }
 );
 
-app.AddCommand("genesis", (
-    String path,
-    Guid? validatorKeyId,
-    PassphraseParameters passphrase
-) =>
-{
-    PrivateKey? validatorKey = null;
-    if (validatorKeyId is { } keyId)
+app.AddCommand(
+    "genesis",
+    (String path, Guid? validatorKeyId, PassphraseParameters passphrase) =>
     {
-        IKeyStore keyStore = Web3KeyStore.DefaultKeyStore;
-        ProtectedPrivateKey protectedKey = keyStore.Get(keyId);
-        while (validatorKey is null)
+        PrivateKey? validatorKey = null;
+        if (validatorKeyId is { } keyId)
         {
-            try
+            IKeyStore keyStore = Web3KeyStore.DefaultKeyStore;
+            ProtectedPrivateKey protectedKey = keyStore.Get(keyId);
+            while (validatorKey is null)
             {
-                validatorKey = protectedKey.Unprotect(
-                    passphrase.Take($"Passphrase ({keyId}): ")
-                );
-            }
-            catch (IncorrectPassphraseException)
-            {
-                Console.Error.WriteLine("Incorrect passphrase.  Try again.");
+                try
+                {
+                    validatorKey = protectedKey.Unprotect(
+                        passphrase.Take($"Passphrase ({keyId}): ")
+                    );
+                }
+                catch (IncorrectPassphraseException)
+                {
+                    Console.Error.WriteLine("Incorrect passphrase.  Try again.");
+                }
             }
         }
+        if (validatorKey is not null)
+        {
+            var gesisBlock = GenesisBlockHelper.ProposeGenesisBlock(
+                new PrivateKey[] { validatorKey },
+                validatorKey
+            );
+            var gesisBlockBytes = GenesisBlockHelper.ExportBlock(gesisBlock);
+            File.WriteAllBytes(path, gesisBlockBytes);
+        }
     }
-    if (validatorKey is not null)
-    {
-        var gesisBlock = GenesisBlockHelper.ProposeGenesisBlock(new PrivateKey[] { validatorKey }, validatorKey);
-        var gesisBlockBytes = GenesisBlockHelper.ExportBlock(gesisBlock);
-        File.WriteAllBytes(path, gesisBlockBytes);
-    }
-});
+);
 
 app.Run();
