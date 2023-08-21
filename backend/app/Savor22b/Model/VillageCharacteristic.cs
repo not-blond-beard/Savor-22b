9d7dc8d837b0ab05d8d5ab76a54cc1b66c9925e9
upa-r-upa <duekthtjf783@gmail.com>
2023-08-21 12:58:26 +0900

    feat: Change village_characteristics CSV Model

--- backend/app/Savor22b/Model/VillageCharacteristic.cs
+++ backend/app/Savor22b/Model/VillageCharacteristic.cs
@@ -1,9 +1,10 @@
+using System.Collections.Immutable;
+
 namespace Savor22b.Model;
 
 public class VillageCharacteristic
 {
-    // ID,VillageID,AvailableSeedID
     public int Id { get; set; }
     public int VillageId { get; set; }
-    public string AvailableSeedId { get; set; }
+    public ImmutableList<int> AvailableSeedIdList { get; set; }
 }
