9d8c1a7d68c7a40eeea1bc3b51a6a459e6df1b0e
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2023-08-25 21:36:34 +0900

    wip : create food but not visible

--- frontend/Savor-22b/Assets/Scripts/Models/QueryNames.cs
+++ frontend/Savor-22b/Assets/Scripts/Models/QueryNames.cs
@@ -4,4 +4,5 @@ public static class QueryNames
 {
     public static readonly string GET_ALL_RECIPE = "GetAllRecipe";
     public static readonly string CREATE_INGREDIENT = "CreateNewIngredient";
+    public static readonly string CREATE_FOOD = "CreateNewFood";
 }
