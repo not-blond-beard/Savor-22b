7153bb349a2d02ed184ab7d1b7aa3c034da0b838
Hyun Seungmin <boscohyun@gmail.com>
2023-12-20 15:31:50 +0900

    move scripts

--- frontend/Saver-22b-godot/scripts/scenes/select_village.gd
+++ frontend/Saver-22b-godot/scripts/scenes/select_village.gd
@@ -11,5 +11,10 @@ func _on_village_button_button_down(extra_arg_0):
 	selected_village_index = extra_arg_0
 
 func _on_start_button_button_down():
-	var format_string = "start button down: %s"
-	print(format_string % selected_village_index)
+	print("start button down: %s" % selected_village_index)
+	_go_to_village(selected_village_index)
+
+func _go_to_village(village_index):
+	var scene_path = "go to village: res://scenes/%s.tscn" % village_index
+	print(scene_path)
+	#get_tree().change_scene_to_file(scene_path)
