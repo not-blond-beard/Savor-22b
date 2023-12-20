1afdc52bcd5491e39503d6109e23e49d793456ab
Hyun Seungmin <boscohyun@gmail.com>
2023-12-20 17:11:34 +0900

    query villages

--- frontend/Saver-22b-godot/scripts/global/scene_context.gd
+++ frontend/Saver-22b-godot/scripts/global/scene_context.gd
@@ -1,52 +1,10 @@
 extends Node
 
-var villages_json_string := """{
-	"villages": [
-	  {
-		"id": 1,
-		"name": "평범한 도시 1",
-		"width": 3,
-		"height": 11,
-		"worldX": 5,
-		"worldY": 1,
-		"houses": [
-		  {
-			"x": 1,
-			"y": 1,
-			"owner": "0x53103C2D7875D2f5f02AeC3075155e268a6e3A94"
-		  }
-		]
-	  },
-	  {
-		"id": 2,
-		"name": "외로운 섬",
-		"width": 7,
-		"height": 13,
-		"worldX": -2,
-		"worldY": 7,
-		"houses": []
-	  },
-	  {
-		"id": 3,
-		"name": "정글",
-		"width": 3,
-		"height": 5,
-		"worldX": 1,
-		"worldY": -3,
-		"houses": []
-	  }
-	]
-  }"""
 var villages: Array
 var selected_village_index := 0
 
-func _ready():
-	var json = JSON.new()
-	var error = json.parse(villages_json_string)
-	if error != OK:
-		print(error)
-	else:
-		villages = json.data.villages
+func set_villages(query_data: Dictionary):
+	villages = query_data.data.villages
 
 func get_selected_village():
 	return villages[selected_village_index]
--- frontend/Saver-22b-godot/scripts/scenes/intro.gd
+++ frontend/Saver-22b-godot/scripts/scenes/intro.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	print("intro scene ready")
+	_query_villages()
 
 func _on_quit_button_button_down():
 	print("quit button down")
@@ -10,3 +11,24 @@ func _on_quit_button_button_down():
 func _on_play_button_button_down():
 	print("play button down")
 	get_tree().change_scene_to_file("res://scenes/select_village.tscn")
+
+func _query_villages():
+	var query = GQLQuery.new("villages").set_props([
+		"id",
+		"name",
+		"width",
+		"height",
+		"worldX",
+		"worldY",
+		GQLQuery.new("houses").set_props([
+			"x",
+			"y",
+			"owner",
+		]),
+	])
+	
+	var query_executor = SvrGqlClient.query('query', {}, query)
+	query_executor.graphql_response.connect(func(data):
+		SceneContext.set_villages(data))
+	add_child(query_executor)
+	query_executor.run({})
