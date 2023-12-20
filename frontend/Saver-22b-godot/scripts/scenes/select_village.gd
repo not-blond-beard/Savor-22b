e90160f0b0aae4899e158b47a682815d6a01a0dc
Hyun Seungmin <boscohyun@gmail.com>
2023-12-20 16:14:37 +0900

    change scene to village_view from select_village

--- frontend/Saver-22b-godot/scripts/scenes/select_village.gd
+++ frontend/Saver-22b-godot/scripts/scenes/select_village.gd
@@ -12,9 +12,5 @@ func _on_village_button_button_down(extra_arg_0):
 
 func _on_start_button_button_down():
 	print("start button down: %s" % selected_village_index)
-	_go_to_village(selected_village_index)
-
-func _go_to_village(village_index):
-	var scene_path = "go to village: res://scenes/%s.tscn" % village_index
-	print(scene_path)
-	#get_tree().change_scene_to_file(scene_path)
+	SceneContext.selected_village_index = selected_village_index
+	get_tree().change_scene_to_file("res://village_view/VillageView.tscn")
