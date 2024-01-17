3b352c376d9baba7bfd770ff1d52ada9e80b418b
Jiwon <me@atralupus.com>
2024-01-17 23:34:34 +0900

    rename and remove old folder (#115)
    * rename and remove old folder
    
    * Add submodule

--- frontend/Savor-22b-old/Assets/Scripts/Models/QueryNames.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public static class QueryNames
-{
-    public static readonly string GET_ALL_RECIPE = "GetAllRecipe";
-    public static readonly string CREATE_INGREDIENT = "CreateNewIngredient";
-    public static readonly string CREATE_FOOD = "CreateNewFood";
-}
