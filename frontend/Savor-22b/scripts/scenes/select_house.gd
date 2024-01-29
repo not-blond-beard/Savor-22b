78911ca5513a57b2d8b5ac92b2db8c0b4ba3d10f
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-01-29 21:37:40 +0900

    feat : home slot location impl (#119)

--- frontend/Savor-22b/scripts/scenes/select_house.gd
+++ frontend/Savor-22b/scripts/scenes/select_house.gd
@@ -8,21 +8,40 @@ const SLOT_IS_FULL = preload("res://ui/notice_popup.tscn")
 
 var houses = []
 
+
 func _ready():
 	print("select_house scene ready")
 	var size = SceneContext.selected_village_capacity
 	testinput()
+
 	
 	gridcontainer.columns = SceneContext.selected_village_width
 	
+	var startxloc = -(( SceneContext.selected_village_width - 1 ) / 2)
+	var startyloc = (SceneContext.selected_village_height -1 ) / 2
+	var endxloc = ( SceneContext.selected_village_width - 1 ) / 2
+	
+	print("startxloc: %s" % startxloc)
+	print("startyloc: %s" % startyloc)
+	print("endxloc: %s" % endxloc)
+
+	
 	
 	for i in range(size):
-		var house = {"x" : i, "y" : 0, "owner" : "none"}
+		var house = {"x" : startxloc, "y" : startyloc, "owner" : "none"}
 		houses.append(house)
 		var button = SELECT_HOUSE_BUTTON.instantiate()
 		button.set_house(house)
 		button.button_down.connect(button_selected)
-		gridcontainer.add_child(button)
+		gridcontainer.add_child(button)		
+		
+		if(startxloc == endxloc):
+			startyloc -= 1
+			startxloc = -(( SceneContext.selected_village_width - 1 ) / 2)
+		else:
+			startxloc += 1
+
+
 	
 	#for house in houses:
 		#var button = SELECT_HOUSE_BUTTON.instantiate()
@@ -54,7 +73,6 @@ func _on_button_pressed():
 func _on_build_button_button_down():
 	print_notice()
 
-	
 	
 func print_notice():
 	var box = SLOT_IS_FULL.instantiate()
