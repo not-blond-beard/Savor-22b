6442fb7844f5367fed445762d1f2f646cbf4725c
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-02-19 15:33:38 +0900

    SVR-178: 밭 전체 화면 및 밭 목록 확인 기능 (#127)
    * fix: farm scene merge error
    
    * feat: view current farms

--- frontend/Savor-22b/scripts/scenes/farm.gd
+++ frontend/Savor-22b/scripts/scenes/farm.gd
@@ -1,4 +1,46 @@
 extends Control
 
+const FARM_SLOT_EMPTY = preload("res://ui/farm_slot_empty.tscn")
+const FARM_SLOT_OCCUPIED = preload("res://ui/farm_slot_button.tscn")
+const FARM_SLOT_DONE = preload("res://ui/farm_slot_done.tscn")
+
+@onready var leftfarm = $MC/HC/CR/MC/HC/Left
+@onready var rightfarm = $MC/HC/CR/MC/HC/Right
+
+var farms = []
+
 func _ready():
 	print("farm scene ready")
+	
+	print(SceneContext.user_state["villageState"]["houseFieldStates"])
+	farms = SceneContext.user_state["villageState"]["houseFieldStates"]
+	
+	#create blank slots
+	for i in range(0,5):
+		var farm
+		if (farms[i] == null):
+			farm = FARM_SLOT_EMPTY.instantiate()
+		else:
+			if(farms[i]["isHarvested"]):
+				farm = FARM_SLOT_DONE.instantiate()
+				farm.set_farm_slot(farms[i])
+			else:
+				farm = FARM_SLOT_OCCUPIED.instantiate()
+				farm.set_farm_slot(farms[i])
+		
+		leftfarm.add_child(farm)
+		
+	for i in range(5,10):
+		var farm
+		if (farms[i] == null):
+			farm = FARM_SLOT_EMPTY.instantiate()
+		else:
+			if(farms[i]["isHarvested"]):
+				farm = FARM_SLOT_DONE.instantiate()
+			else:
+				farm = FARM_SLOT_OCCUPIED.instantiate()
+		
+		rightfarm.add_child(farm)	
+	
+	
+	
