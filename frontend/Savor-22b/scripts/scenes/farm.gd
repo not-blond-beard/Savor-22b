5b9a797f196502222ecc76b4d49914d00f1541d1
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-02-24 01:12:31 +0900

    SVR-285: 종자 설치 기능 (#130)
    * feat: farm index selector
    
    * feat: plantseed query code
    
    * feat: read seedticket
    
    * feat: install popup
    
    * fix: useless lines and uis from farmslot
    
    * feat: install seed in ui
    
    * feat: show popup with amount

--- frontend/Savor-22b/scripts/scenes/farm.gd
+++ frontend/Savor-22b/scripts/scenes/farm.gd
@@ -4,10 +4,18 @@ const FARM_SLOT_EMPTY = preload("res://ui/farm_slot_empty.tscn")
 const FARM_SLOT_OCCUPIED = preload("res://ui/farm_slot_button.tscn")
 const FARM_SLOT_DONE = preload("res://ui/farm_slot_done.tscn")
 
+const INSTALL_POPUP = preload("res://ui/farm_install_popup.tscn")
+
+const Gql_query = preload("res://gql/query.gd")
+
 @onready var leftfarm = $MC/HC/CR/MC/HC/Left
 @onready var rightfarm = $MC/HC/CR/MC/HC/Right
 
+@onready var popuparea = $Popups
+
 var farms = []
+var itemStateIds = []
+var itemStateIdToUse
 
 func _ready():
 	print("farm scene ready")
@@ -15,11 +23,16 @@ func _ready():
 	print(SceneContext.user_state["villageState"]["houseFieldStates"])
 	farms = SceneContext.user_state["villageState"]["houseFieldStates"]
 	
+	print(SceneContext.user_state["inventoryState"]["itemStateList"])
+	itemStateIds = SceneContext.user_state["inventoryState"]["itemStateList"]
+	
 	#create blank slots
 	for i in range(0,5):
 		var farm
 		if (farms[i] == null):
 			farm = FARM_SLOT_EMPTY.instantiate()
+			farm.im_left()
+			farm.button_down.connect(farm_selected)
 		else:
 			if(farms[i]["isHarvested"]):
 				farm = FARM_SLOT_DONE.instantiate()
@@ -34,13 +47,63 @@ func _ready():
 		var farm
 		if (farms[i] == null):
 			farm = FARM_SLOT_EMPTY.instantiate()
+			farm.im_right()
+			farm.button_down.connect(farm_selected)
 		else:
 			if(farms[i]["isHarvested"]):
 				farm = FARM_SLOT_DONE.instantiate()
+				farm.set_farm_slot(farms[i])				
 			else:
 				farm = FARM_SLOT_OCCUPIED.instantiate()
+				farm.set_farm_slot(farms[i])				
 		
-		rightfarm.add_child(farm)	
+		rightfarm.add_child(farm)
 	
 	
+func farm_selected(farm_index):
+	var format_string = "farm selected: %s"
+	print(format_string % farm_index)
+	SceneContext.selected_field_index = farm_index
 	
+	plant_popup()
+
+func plant_popup():
+	if is_instance_valid(popuparea):
+		for child in popuparea.get_children():
+			child.queue_free()
+	
+	var amount = itemStateIds.size()
+	print(amount)
+	var mousepos = get_local_mouse_position() + Vector2(0, -200)
+	var installpopup = INSTALL_POPUP.instantiate()
+	installpopup.set_amount(amount)
+	popuparea.add_child(installpopup)
+	installpopup.set_position(mousepos)
+	installpopup.accept_button_down.connect(plant_seed)
+	
+
+func plant_seed():
+	var gql_query = Gql_query.new()
+	var query_string = gql_query.plant_seed_query_format.format([
+		"\"%s\"" % GlobalSigner.signer.GetPublicKey(),
+		SceneContext.selected_field_index,
+		"\"%s\"" % itemStateIds[0]["stateID"]], "{}")
+	print(query_string)
+	
+	var query_executor = SvrGqlClient.raw(query_string)
+	query_executor.graphql_response.connect(func(data):
+		print("gql response: ", data)
+		var unsigned_tx = data["data"]["createAction_PlantingSeed"]
+		print("unsigned tx: ", unsigned_tx)
+		var signature = GlobalSigner.sign(unsigned_tx)
+		print("signed tx: ", signature)
+		var mutation_executor = SvrGqlClient.raw_mutation(gql_query.stage_tx_query_format % [unsigned_tx, signature])
+		mutation_executor.graphql_response.connect(func(data):
+			print("mutation res: ", data)
+		)
+		add_child(mutation_executor)
+		mutation_executor.run({})
+	)
+	add_child(query_executor)
+	query_executor.run({})
+
---
+++ frontend/Savor-22b/ui/farm_slot_empty.gd
@@ -0,0 +1,24 @@
+extends ColorRect
+
+signal button_down(child_index: int)
+
+var isleft: bool
+
+func _ready():
+	pass
+
+func _on_button_button_down():
+	
+	# return index of selected slot
+	if (isleft):
+		button_down.emit(get_index())
+	else:
+		button_down.emit(get_index()+5)
+		
+
+
+func im_right():
+	isleft = false
+
+func im_left():
+	isleft = true
