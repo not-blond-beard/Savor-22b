5f6558834e8fbf2fa9248a108aa5089209ea9384
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-03-01 14:02:21 +0900

    feat: system shop item query

--- frontend/Savor-22b/scenes/testpanel/test_panel.gd
+++ frontend/Savor-22b/scenes/testpanel/test_panel.gd
@@ -1,8 +1,9 @@
 extends Control
 
+@onready var label = $Background/MarginContainer/GridContainer/Label
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -13,3 +14,9 @@ func _input(event):
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/farm.tscn")
+
+
+func _on_button_2_pressed():
+	
+	var test = SceneContext.shop
+	print(test)
