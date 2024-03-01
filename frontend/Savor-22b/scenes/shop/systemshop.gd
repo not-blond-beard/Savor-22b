2164223cc27f79298e1d3ba624d440612284afcd
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-03-01 15:51:14 +0900

    feat: shop item buy query

--- frontend/Savor-22b/scenes/shop/systemshop.gd
+++ frontend/Savor-22b/scenes/shop/systemshop.gd
@@ -4,6 +4,8 @@ const ITEM = preload("res://scenes/shop/shop_item.tscn")
 const SHOP_ASK_POPUP = preload("res://scenes/shop/ask_popup.tscn")
 const SHOP_DONE_POPUP = preload("res://scenes/shop/done_popup.tscn")
 
+const Gql_query = preload("res://gql/query.gd")
+
 @onready var shoplist = $M/H/C/M/Lists
 @onready var popup = $Popups
 
@@ -21,6 +23,7 @@ func _ready():
 		singleitem.set_item(item)
 		singleitem.button_down.connect(buy_item)
 		shoplist.add_child(singleitem)
+		
 
 
 func buy_item():
@@ -29,6 +32,31 @@ func buy_item():
 	
 	popup.add_child(askpopup)
 	
+
+func buyquery():
+	var itemnum = 1
+	var gql_query = Gql_query.new()
+	var query_string = gql_query.buy_shop_item_query_format.format([
+		"\"%s\"" % GlobalSigner.signer.GetPublicKey(),
+		itemnum], "{}")
+	print(query_string)
+	
+	var query_executor = SvrGqlClient.raw(query_string)
+	query_executor.graphql_response.connect(func(data):
+		print("gql response: ", data)
+		var unsigned_tx = data["data"]["createAction_BuyShopItem"]
+		print("unsigned tx: ", unsigned_tx)
+		var signature = GlobalSigner.sign(unsigned_tx)
+		print("signed tx: ", signature)
+		var mutation_executor = SvrGqlClient.raw_mutation(gql_query.stage_tx_query_format % [unsigned_tx, signature])
+		mutation_executor.graphql_response.connect(func(data):
+			print("mutation res: ", data)
+		)
+		add_child(mutation_executor)
+		mutation_executor.run({})
+	)
+	add_child(query_executor)
+	query_executor.run({})
 
 func _on_close_button_down():
 	queue_free()
