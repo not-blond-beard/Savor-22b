45633805b9b9d42f618947afbbb18e4d0c83be8e
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-03-14 23:48:28 +0900

    ui: added recipebook ui components

--- frontend/Savor-22b/gql/svr_gql_client.gd
+++ frontend/Savor-22b/gql/svr_gql_client.gd
@@ -2,9 +2,10 @@ extends GQLClient
 
 func _ready():
 	print("client ready")
-	#set_endpoint(false, "192.168.2.9", 38080, "/graphql")
+	set_endpoint(false, "1.230.253.103", 38080, "/graphql")
 	
-	set_endpoint(false, "localhost", 38080, "/graphql")
+	#set_endpoint(false, "localhost", 38080, "/graphql")
+	
 
 func raw_mutation(query:String):
 	return GQLQueryExecuter.new(endpoint, use_ssl, Mutation.new(query))
