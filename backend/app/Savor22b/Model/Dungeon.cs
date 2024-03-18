286802ebdacad98df9c1966fb985e924692dff28
upa-r-upa <me.uparupa@gmail.com>
2024-03-18 18:57:47 +0900

    feat: Add dungeon CSV and model

---
+++ backend/app/Savor22b/Model/Dungeon.cs
@@ -0,0 +1,10 @@
+namespace Savor22b.Model;
+
+public class Dungeon
+{
+    public string Name { get; set; }
+    public int Y { get; set; }
+    public int X { get; set; }
+    public int ID { get; set; }
+    public int VillageId { get; set; }
+}
