4ab675c2ccfcf673560b704bf5be8341e4b7490a
Jiwon <me@atralupus.com>
2024-04-14 17:51:31 +0900

    [SVR-301] 상품 구매 액션 추가 (#159)
    * Add buy trade good action
    
    * Add query

--- backend/app/Savor22b/GraphTypes/Query/Query.cs
+++ backend/app/Savor22b/GraphTypes/Query/Query.cs
@@ -578,6 +578,38 @@ public class Query : ObjectGraphType
         );
 
         Field<NonNullGraphType<StringGraphType>>(
+            "createAction_BuyTradeGoodAction",
+            description: "무역상점 구매",
+            arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<StringGraphType>>
+                {
+                    Name = "publicKey",
+                    Description = "The base64-encoded public key for Transaction.",
+                },
+                new QueryArgument<NonNullGraphType<GuidGraphType>>
+                {
+                    Name = "productId",
+                    Description = "상품 고유 Id",
+                }
+            ),
+            resolve: context =>
+            {
+                var publicKey = new PublicKey(
+                    ByteUtil.ParseHex(context.GetArgument<string>("publicKey"))
+                );
+
+                var action = new BuyTradeGoodAction(context.GetArgument<Guid>("productId"));
+
+                return new GetUnsignedTransactionHex(
+                    action,
+                    publicKey,
+                    _blockChain,
+                    _swarm
+                ).UnsignedTransactionHex;
+            }
+        );
+
+        Field<NonNullGraphType<StringGraphType>>(
             "createAction_RegisterTradeGoodAction",
             description: "Register Trade Good to Trade Store",
             arguments: new QueryArguments(
