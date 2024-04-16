455c0e4df4fcf858d747041b24ce4d36f9a4bf9f
Atralupus <me@atralupus.com>
2024-04-17 01:42:43 +0900

    Handle Buy dungeon conquest

--- backend/app/Savor22b.Tests/Action/BuyTradeGoodTests.cs
+++ backend/app/Savor22b.Tests/Action/BuyTradeGoodTests.cs
@@ -15,9 +15,9 @@ public class BuyTradeGoodTests : ActionTests
     public BuyTradeGoodTests() { }
 
     [Fact]
-    public void Execute_Success()
+    public void Execute_Success_Food()
     {
-        var (stateDelta, productId) = CreatePresetStateDelta();
+        var (stateDelta, productId, _) = CreatePresetStateDelta();
         stateDelta = stateDelta.MintAsset(
             SignerAddress(),
             FungibleAssetValue.Parse(Currencies.KeyCurrency, "10")
@@ -41,14 +41,51 @@ public class BuyTradeGoodTests : ActionTests
         var afterTradeInventoryState = DeriveTradeInventoryStateDelta(stateDelta);
         var afterRootState = DeriveRootStateFromAccountStateDelta(stateDelta);
 
-        Assert.Empty(afterTradeInventoryState.TradeGoods);
+        Assert.True(afterTradeInventoryState.TradeGoods.Count == 1);
         Assert.True(afterRootState.InventoryState.RefrigeratorStateList.Count == 1);
+    }
+
+    [Fact]
+    public void Execute_Success_DungeonConquest()
+    {
+        var (stateDelta, _, productId) = CreatePresetStateDelta();
+        stateDelta = stateDelta.MintAsset(
+            SignerAddress(),
+            FungibleAssetValue.Parse(Currencies.KeyCurrency, "10")
+        );
+
+        var action = new BuyTradeGoodAction(
+            productId
+        );
+
+        stateDelta = action.Execute(
+            new DummyActionContext
+            {
+                PreviousStates = stateDelta,
+                Signer = SignerAddress(),
+                Random = random,
+                Rehearsal = false,
+                BlockIndex = 10,
+            }
+        );
+
+        var afterTradeInventoryState = DeriveTradeInventoryStateDelta(stateDelta);
+        var afterRootState = DeriveRootStateFromAccountStateDelta(stateDelta);
+        var afterGlobalDungeonState = DeriveGlobalDungeonStateDelta(stateDelta);
+
+        Assert.True(afterTradeInventoryState.TradeGoods.Count == 1);
+        Assert.True(afterRootState.UserDungeonState.DungeonConquestHistories.Count == 1);
+
+        var dungeonConquest = afterRootState.UserDungeonState.DungeonConquestHistories[0];
+        Assert.True(dungeonConquest.BlockIndex == 10);
+        Assert.True(dungeonConquest.DungeonConquestStatus == 1);
+        Assert.True(afterGlobalDungeonState.IsDungeonConquestAddress(dungeonConquest.DungeonId, SignerAddress()));
     }
 
     [Fact]
     public void Execute_Fail_InsufficientBalance()
     {
-        var (stateDelta, productId) = CreatePresetStateDelta();
+        var (stateDelta, productId, _) = CreatePresetStateDelta();
 
         var action = new BuyTradeGoodAction(
             productId
@@ -69,7 +106,7 @@ public class BuyTradeGoodTests : ActionTests
         });
     }
 
-    private (IAccountStateDelta, Guid) CreatePresetStateDelta()
+    private (IAccountStateDelta, Guid, Guid) CreatePresetStateDelta()
     {
         IAccountStateDelta state = new DummyState();
         Address signerAddress = SignerAddress();
@@ -83,6 +120,7 @@ public class BuyTradeGoodTests : ActionTests
             : new TradeInventoryState();
 
         var foodProductId = Guid.NewGuid();
+        var dungeonConquestProductId = Guid.NewGuid();
 
         var food = RefrigeratorState.CreateFood(
             Guid.NewGuid(),
@@ -103,7 +141,15 @@ public class BuyTradeGoodTests : ActionTests
                 FungibleAssetValue.Parse(Currencies.KeyCurrency, "10"),
                 food)
         );
+        tradeInventoryState = tradeInventoryState.RegisterGood(
+            new DungeonConquestGoodState(
+                signerAddress,
+                dungeonConquestProductId,
+                FungibleAssetValue.Parse(Currencies.KeyCurrency, "10"),
+                1)
+        );
+
         state = state.SetState(TradeInventoryState.StateAddress, tradeInventoryState.Serialize());
-        return (state.SetState(signerAddress, rootState.Serialize()), foodProductId);
+        return (state.SetState(signerAddress, rootState.Serialize()), foodProductId, dungeonConquestProductId);
     }
 }
