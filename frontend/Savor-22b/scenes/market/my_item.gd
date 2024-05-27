4ef0f13349d9c2239369669b345cc40ef857d588
Tae Hyoung Kang <78776542+kth1888@users.noreply.github.com>
2024-05-28 01:25:20 +0900

    fix : ui problems #2

--- frontend/Savor-22b/scenes/market/my_item.gd
+++ frontend/Savor-22b/scenes/market/my_item.gd
@@ -1,9 +1,8 @@
 extends Control
 
-@onready var item_button = $ItemSelectButton
-@onready var item_name_label = $ItemSelectButton/MarginContainer/VBoxContainer/HBoxContainer/NameLabel
-@onready var item_grade_label = $ItemSelectButton/MarginContainer/VBoxContainer/HBoxContainer/GradeLabel
-@onready var item_stateid_label = $ItemSelectButton/MarginContainer/VBoxContainer/StateIdLabel
+@onready var item_name_label = $Background/MarginContainer/VBoxContainer/NameLabel
+@onready var item_grade_label = $Background/MarginContainer/VBoxContainer/HBoxContainer/GradeLabel
+@onready var item_amount_label = $Background/MarginContainer/VBoxContainer/HBoxContainer/AmountLabel
 
 var info
 
@@ -11,15 +10,15 @@ func _ready():
 	_update_info()
 
 func _update_info():
-	if item_button == null:
+	if $Background == null:
 		return
 
-	item_name_label.text = info.name
-	item_grade_label.text = info.grade + "등급"
-	item_stateid_label.text = info.stateId
+	item_name_label.text = info[0]["name"]
+	item_grade_label.text = info[0]["grade"] + "등급"
+	item_amount_label.text = "%s 개" % [info.size()]
 
-func set_info(info: Dictionary):
+func set_info(info: Array):
 	self.info = info
 
-func _on_item_select_button_down():
+func _on_register_trade_button_down():
 	pass # 물건 올리기 기능이 들어갈 예정
---
+++ frontend/Savor-22b/scenes/market/my_trade_goods.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var item_button = $ItemSelectButton
+@onready var description_label = $BackgroundPanel/MarginContainer/VBoxContainer/DescriptionLabel
+
+var info
+var desc_format_string = "%s : %s
+%s : %s
+%s : %s%s"
+
+func _ready():
+	_update_info()
+
+func _update_info():
+	if description_label == null:
+		return
+	description_label.text = desc_format_string % ["품목명",info.food["name"],"stateId",info.food["stateId"],"가격",info.price," BBG"]
+
+func set_info(info: Dictionary):
+	self.info = info
